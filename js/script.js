bbd156a626e1697cdbb0e8f97f1b0714703f5312
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2021-12-13 15:22:33 +0900

    작업 끝

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
   const $sec = document.querySelectorAll('section');
+  console.log($sec);
   const $fullLogo = document.querySelector('#full-logo');
 
   const $blue = document.querySelector('#bluebox');
@@ -22,9 +23,23 @@ window.addEventListener('load', () => {
   console.log($gentBox);
 
   const $imgs = document.querySelectorAll('#img>div');
+  const $texts = document.querySelectorAll('#txt>div');
+  console.log($texts)
 
 
   $fullLogo.classList.replace('opa-0', 'opa-1')
+  
+  setInterval(() => {
+    if($sec[0].classList.contains('opa-1')) {
+      $sec[0].classList.replace('opa-1','opa-0');
+      $sec[1].classList.replace('opa-0','opa-1');
+    } else {
+      $sec[1].classList.replace('opa-1','opa-0');
+      $sec[0].classList.replace('opa-0','opa-1');
+    }
+  }, 3000);
+
+
   window.addEventListener('scroll' , () => {
 
     // console.log($sec[3].getBoundingClientRect());
@@ -45,7 +60,9 @@ window.addEventListener('load', () => {
     }, 500);
     setInterval(() => {
       $celH2.style.opacity = 1;
+      $celH2.style.transform = "scale(1)";
       $celP.style.opacity = 1;
+      $celP.style.transform = "scale(1)";
     }, 1000);
   }
 
@@ -57,7 +74,9 @@ window.addEventListener('load', () => {
     }, 500);
     setInterval(() => {
       $alaH2.style.opacity = 1;
+      $alaH2.style.transform = "scale(1)";
       $alaP.style.opacity = 1;
+      $alaP.style.transform = "scale(1)";
     }, 1000);
   }
 
@@ -67,10 +86,12 @@ window.addEventListener('load', () => {
       $genfl.classList.replace('opa-0','opa-1');
       $genfl.classList.add('bo-rad35');
       $gentTxt.classList.replace('opa-0','opa-1');
+      $gentTxt.style.transform = "scale(1)";
       for(let i = 0; i<$gentBox.length; i++) {
         setInterval(() => {
           $gentBox[i].classList.replace('opa-0','opa-1');
-        }, (500) * (i+1));
+          $gentBox[i].style.transform = "scale(1)";
+        }, (700) * (i+1));
       }
       }, 800);
       
@@ -78,14 +99,14 @@ window.addEventListener('load', () => {
 
   if (scrollY >= 2900) {
     $setumei.classList.replace('opa-0','opa-1');
-    $setumei.style.top = "50vh";
+    $setumei.style.top = "75vh";
   }
 
-  if (scrollY >= 3000) {
+  if (scrollY >= 3100) {
     for(let i = 0; i < $imgs.length; i++) {
       setInterval(() => {
         $imgs[i].classList.replace('opa-0','opa-1');
-        $imgs[i].style.top = "-50vh";
+        $texts[i].classList.replace('opa-0','opa-1');
       }, (500) * (i+1));
     }
   }
